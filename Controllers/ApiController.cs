a309e402babb92d486e56e831b49b245c5b94269
啟翔 黃 <max74107410@gmail.com>
2022-07-04 10:31:02 +0800

    0704

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSIT141Ajax.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello~~Ajax</h1>","text/html",System.Text.Encoding.UTF8);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace MSIT141Ajax
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=index}/{id?}");
             });
         }
     }
